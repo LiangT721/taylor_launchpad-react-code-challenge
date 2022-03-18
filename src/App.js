31bf6b24e6c79737b1ba87450724f5681d9b710f
TangLian <96833954+LiangT721@users.noreply.github.com>
2022-03-18 16:32:12 -0600

    Create home page from homepage-branch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import "./App.scss";
 
+import { Routes, Route } from "react-router-dom";
+
 import Nav from "./components/nav/nav.component";
+import Homepage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
-      <div className="app container conntainer-xxl">
-        <div className="row">
-          <Nav />
-          <div className="pages col-11">
-          </div>
+    <div className="app container conntainer-xxl">
+      <div className="row">
+        <Nav />
+        <div className="pages col-11">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+          </Routes>
         </div>
       </div>
+    </div>
   );
 }
 
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import './homepage.component';
+
+const Homepage = () => {
+
+    return(
+        <div className="homepage">
+        Home page
+        </div>
+    )
+}
+
+export default Homepage;
