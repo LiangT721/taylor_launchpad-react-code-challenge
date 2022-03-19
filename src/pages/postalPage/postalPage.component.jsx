f03e48364e83fc9cab9b69f86e2f16bb778ee210
TangLian <96833954+LiangT721@users.noreply.github.com>
2022-03-19 13:39:46 -0600

    Created postal lookup page from postal branch

---
+++ src/pages/postalPage/postalPage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import './postalPage.style.scss'
+
+const PostalPage = () => {
+    return(
+        <div className="postal-page">
+        postal lookup</div>
+    )
+}
+
+export default PostalPage;
