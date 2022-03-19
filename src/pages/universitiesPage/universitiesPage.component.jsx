4f20e4cdbd3686f34878f3f8ab0aac5d02c613cf
TangLian <96833954+LiangT721@users.noreply.github.com>
2022-03-19 09:27:19 -0600

    Created Universities Page from universities-page branch

---
+++ src/pages/universitiesPage/universitiesPage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import './universitiesPage.style.scss';
+
+const UniversitiesPage = () => {
+    return(
+        <div className="universities-page">
+        Universities page
+        </div>
+    )
+}
+export default UniversitiesPage;
